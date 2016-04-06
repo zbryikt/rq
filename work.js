29a3f4b036413cffac275cd1cbd76905e9e52a3b
Kirby T. Wu <tkirby@gmail.com>
2016-04-06 16:07:15 +0800

    update

--- work.js
+++ work.js
@@ -4,7 +4,8 @@ Chart = function() { return this; };
 Chart.prototype = {
   config: {
     margin: 10,
-    bubbleMode: false
+    fontSize: 12,
+    bubbleMode: true
   },
   preinit: function() {
     this.root = document.getElementById("container");
@@ -29,10 +30,12 @@ Chart.prototype = {
     var that = this;
     this.popup = d3.select(this.root).select("#popup");
     this.svg = d3.select(this.root).select("svg");
-    this.calendar = this.svg.append("g").attr({class: "calendar"})
+    this.navByDate = this.svg.append("g").attr({class: "navByDate"});
+    this.calendar = this.svg.append("g").attr({class: "calendar"});
     this.xAxisGroup = this.svg.append("g").attr({class: "axis horizontal"});
     this.popdonut = this.svg.append("g").attr({class: "popup-donut"});
     this.popdonut.selectAll("path.ratio").data([0,0,0,0,0,0]).enter().append("path").attr({class: "ratio"});
+    this.offset = 0;
     this.parsed = d3.nest().key(function(it) {
       return parseInt((it.date.getTime() - it.date.getDay() * 86400 * 1000) / 86400000) * 86400000;
     }).entries(this.data);
@@ -42,6 +45,14 @@ Chart.prototype = {
         return it.date.getDay();
       }).entries(week.values);
     });
+    this.dates = d3.nest().key(function(it) { return it[0]; }).entries(
+      this.data.map(function(it) { return [it.date.getYear() + 1900, it.date.getMonth() + 1]})
+    )
+    this.dates.sort(function(a,b) { return b.key - a.key; });
+    this.dates.forEach(function(it) { it.values = d3.map(it.values, function(it) { return it[1]; }).keys(); });
+    this.dates.forEach(function(d,i) { d.values.sort(function(a,b) { return b - a; }); });
+
+    this.activeDate = this.dates[0];
   },
   pad: function(v, len) {
     if(typeof(len)=="undefined") len = 2;
@@ -91,15 +102,8 @@ Chart.prototype = {
       .each(function(d,i) {
         var node = d3.select(this);
         var pie = node.append("g").attr({class: "pie"});
-        var sum = d.values.reduce(function(a,b) { return a + b.value; }, 0)
-        d.radius = that.rscale(sum);
         d.values.forEach(function(it) { delete it.parent; });
-        d.values.map(function(it,i) {
-          it.a = it.x;
-          it.da = it.dx;
-          it.outradius = d.radius;
-        });
-        that.pack.size([d.radius * 2, d.radius * 2]).nodes({children: d.values});
+
         pie.selectAll("path.data").data(d.values).enter().append("path").attr({class: "data"})
         .on("click", function(data,i) {
           var box = that.popup[0][0].getBoundingClientRect();
@@ -107,13 +111,13 @@ Chart.prototype = {
             position: "absolute",
             top: ( d3.event.clientY + document.body.scrollTop + data.r + 25) + "px",
             left: ( d3.event.clientX - box.width / 2) + "px",
+            display: "block"
           });
           popdonut = that.popdonut[0][0];
           popdonut.parentNode.removeChild(popdonut);
           this.parentNode.appendChild(popdonut);
           that.partition({children: data.ratio});
           that.popdonut.selectAll("path.ratio").data(data.ratio).attr({
-
             fill: function(d,i) {
               return d3.rgb(that.color(data.category)).darker(3).brighter(i).toString();
             },
@@ -141,19 +145,55 @@ Chart.prototype = {
         })
       });
     });
+    this.navByDate.selectAll("g.navByYear").data(this.dates)
+    .enter().append("g").attr({class: "navByYear"}).on("click", function(d) {
+      that.activeDate = d;
+      that.render();
+    }).each(function(d,i) {
+      var node = d3.select(this);
+      node.selectAll("g.navByMonth").data(d.values)
+      .enter().append("g").attr({class: "navByMonth"}).on("click", function(month) {
+        var date = new Date(that.activeDate.key, month).getTime();
+        var lastoffset = document.body.scrollTop;
+        var offset = that.parsed.filter(function(d,i) { return parseInt(d.key) >date }).length * that.weekHeight;
+        var maxoffset = document.body.getBoundingClientRect().height - window.screen.height;
+        offset += (that.box.top + that.xAxisHeight + that.config.margin - 20);
+        //that.offset = offset = (offset > maxoffset ? maxoffset : offset);
+        d3.select(document.body).transition().duration(1000).tween("scrolltop", function() {
+          return function(t) {
+            this.scrollTop = (1 - t) * lastoffset + t * offset;
+          }
+        });
+      }).each(function(d,i) {
+        var node = d3.select(this);
+        node.append("rect").attr({class: "month"});
+        node.append("text").attr({class: "month"});
+      });
+      node.append("rect").attr({class: "year"});
+      node.append("text").attr({class: "year"});
+    });
   },
   resize: function() {
     var that = this;
     this.box = this.root.getBoundingClientRect();
+    this.width = this.box.width;
     this.maxIndex = 200; // experience value
     this.weekHeight = 145; // suggested minimum: 135
     this.radius = 40; // 0 for auto
-    this.dayWidth = 120; // 0 for auto
+    this.navDateWidth = 120;
     this.weekPadding = 10;
     this.dayPadding = 10;
     this.labelsPadding = 20;
     this.labelsWidth = 140;
     this.xAxisHeight = 60;
+    // hard coded day width - not recommend
+    //this.dayWidth = 0; // 0 for auto
+    //var calWidth = this.width - this.navDateWidth - this.labelsWidth - 2 * this.config.margin - this.labelsPadding;
+    //this.dayWidth = (this.dayWidth * 7 > calWidth ? calWidth / 7 : this.dayWidth);
+    this.dayWidth = (
+        this.width - this.navDateWidth - this.labelsWidth 
+      - 2 * this.config.margin - this.labelsPadding * 2
+    ) / 7;
     this.height = (this.weekHeight * ( this.parsed.length + 1) + this.xAxisHeight + 2 * this.config.margin);
     this.height = (this.height > this.box.height ? this.height : this.box.height);
     this.radius = (this.radius > (this.weekHeight - 2 * this.weekPadding ) / 2
@@ -162,9 +202,9 @@ Chart.prototype = {
     );
     this.dayWidth = (this.dayWidth - 2 * this.dayPadding >= this.radius * 2? this.dayWidth : this.radius * 2);
     this.svg.attr({
-      width: this.box.width,
+      width: this.width,
       height: this.height,
-      viewBox: [0,0,this.box.width,this.height].join(" ")
+      viewBox: [0,0,this.width,this.height].join(" ")
     });
     this.arc = d3.svg.arc().innerRadius(0).outerRadius(this.radius);
     this.partition = d3.layout.partition().size([Math.PI * 2, 1]);
@@ -180,6 +220,21 @@ Chart.prototype = {
     this.color = d3.scale.ordinal().range(["#009999","#ffaa00","#ff0000"]);
     this.rscale = d3.scale.linear().domain([0,this.maxIndex]).range([0,this.radius]);
     this.pack = d3.layout.pack().size([this.dayWidth, this.weekHeight]).padding(6);
+
+
+    this.parsed.map(function(d,i) {
+      d.days.map(function(d,i) {
+        var sum = d.values.reduce(function(a,b) { return a + b.value; }, 0)
+        d.radius = that.rscale(sum);
+        d.values.map(function(it,i) {
+          it.a = it.x;
+          it.da = it.dx;
+          it.outradius = d.radius;
+        });
+        that.pack.size([d.radius * 2, d.radius * 2]).nodes({children: d.values});
+      });
+    });
+
   },
   render: function() {
     var that = this;
@@ -273,8 +328,11 @@ Chart.prototype = {
               ? that.arc
                   .startAngle(d.a * (1 - t))
                   .endAngle(d.a + d.da + (Math.PI * 2 - d.a - d.da) * (t))
+                  .startAngle(0)
+                  .endAngle(Math.PI * 2)
                   .innerRadius(0)
-                  .outerRadius(data.radius + (d.r - data.radius) * t)()
+                  .outerRadius(data.radius + (d.r - data.radius) * t)
+                  .outerRadius(d.r)()
               : that.arc
                   .startAngle(d.a)
                   .endAngle(d.a + d.da)
@@ -282,6 +340,56 @@ Chart.prototype = {
                   .outerRadius(data.radius)()
             );
           }
+        });
+      });
+    });
+
+    this.navByDate.attr({
+      transform: "translate(" + (that.width - that.config.margin - that.navDateWidth) + "," + that.offset + ")"
+    }).selectAll("g.navByYear").transition().duration(1000).attr({
+      transform: function(d,i) {
+        var offset = (that.activeDate.key > d.key
+          ?  that.activeDate.values.length * 25
+          : 0
+        );
+        return [
+          "translate(",
+          0,
+          that.xAxisHeight + that.config.margin + i * 25 + offset,
+        ")"].join(" ")
+      }
+    }).each(function(data,i) {
+      var node = d3.select(this);
+      node.select("rect.year").attr({
+        x: 0, y: 0, width: that.navDateWidth, height: 23,
+        fill: "rgba(0,0,0,0.2)"
+      });
+      node.select("text.year").text(data.key).attr({
+        "dominant-baseline": "hanging",
+        "font-size": that.config.fontSize * 1.2,
+        dx: 5, dy: 5
+      });
+      node.selectAll("g.navByMonth").transition().duration(1000)
+      .attr({
+        transform: function(d,i) {
+          return ["translate(",
+            0,
+            (that.activeDate.key == data.key ? (i + 1) * 25 : 0),
+          ")"].join(" ");
+        },
+        opacity: function(d,i) {
+          return (that.activeDate.key == data.key ? 1 : 0)
+        }
+      }).each(function(d,i) {
+        var node = d3.select(this);
+        node.select("rect.month").attr({
+          x: 0, y: 0, width: that.navDateWidth * 0.9, height: 23,
+          fill: "rgba(0,0,0,0.2)"
+        });
+        node.select("text.month").text(d+"月").attr({
+          "dominant-baseline": "hanging",
+          "font-size": that.config.fontSize * 1.2,
+          dx: 5, dy: 5
         });
       });
     });
@@ -299,4 +407,21 @@ setTimeout(function() {
   chart.config.bubbleMode = true;
   chart.render();
 },1000);
+window.addEventListener("resize", function(it) {
+  chart.resize();
+  chart.bind();
+  chart.render();
+});
+
+window.addEventListener("scroll", function(it) {
+  if(chart.scrollHandler) clearTimeout(chart.scrollHandler);
+  chart.offset = document.body.scrollTop;
+  chart.navByDate.attr({
+    transform: [
+      "translate(",
+      (chart.width - chart.config.margin - chart.navDateWidth),
+      chart.offset,
+    ")"].join(" ")
+  });
+});
 }); //endoffile
